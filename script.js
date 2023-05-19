af8b5a3d0eeee26b515ec42379b4aa922b444a06
HEGGIX <glebermakovich@gmail.com>
2023-05-20 01:35:35 +0300

    refactor: add mines

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var rows = 8;
 var columns = 8;
 
 var minesCount = 10;
-var minesLocation = []; // "2-2", "3-4", "2-1"
+var minesLocation = [];
 
-var tilesClicked = 0; //goal to click all tiles except the ones containing mines
+var tilesClicked = 0; 
 var flagEnabled = false;
 
 var gameOver = false;
@@ -20,4 +20,40 @@ flag.textContent =  "🚩";
 
 window.onload = function() {
     startGame();
+}
+
+function setMines() {
+    let minesLeft = minesCount;
+    while (minesLeft > 0) { 
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        let id = r.toString() + "-" + c.toString();
+
+        if (!minesLocation.includes(id)) {
+            minesLocation.push(id);
+            minesLeft -= 1;
+        }
+    }
+}
+
+
+function startGame() {
+    document.getElementById("mines-count").innerText = minesCount;
+    flag.addEventListener("click", setFlag);
+    setMines();
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.addEventListener("click", clickTile);
+            mainBoard.append(tile);
+            row.push(tile);
+        }
+        board.push(row);
+    }
+
+    console.log(board);
 }
